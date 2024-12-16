3c40ccdb9d54afc372b9f09aad2138f9893dbe3d
Jonathan Drumond <jonathandrumond03@gmail.com>
2024-12-16 19:10:42 -0300

    Removendo bugs da interface

--- src/main/java/adapters/Hospede.java
+++ src/main/java/adapters/Hospede.java
@@ -1,5 +1,8 @@
 package adapters;
 
+import javafx.scene.Node;
+import javafx.scene.shape.Circle;
+
 import java.util.concurrent.Semaphore;
 import java.util.Random;
 
@@ -9,14 +12,14 @@ public class Hospede extends Thread {
     private static final Semaphore tvSemaphore = new Semaphore(MAX_HOSPEDES_NA_TV);
     private static int canalAtual = -1;
     private static int espectadoresAssistindo = 0;
-    private String nome;
+    private int id;
     private int canalFavorito;
     private int tempoAssistindoTv;
     private int tempoDescansando;
     private Random random = new Random();
 
-    public Hospede(String nome, int canalFavorito, int tempoAssistindoTv, int tempoDescansando) {
-        this.nome = nome;
+    public Hospede(int id, int canalFavorito, int tempoAssistindoTv, int tempoDescansando) {
+        this.id = id;
         this.canalFavorito = canalFavorito;
         this.tempoAssistindoTv = tempoAssistindoTv;
         this.tempoDescansando = tempoDescansando;
@@ -25,21 +28,21 @@ public class Hospede extends Thread {
     // Método que simula o hóspede assistindo TV
     public void assistirTv() {
         boolean conseguiuAssistir = false;
-
         while (!conseguiuAssistir) {
             synchronized (Hospede.class) {
                 if (espectadoresAssistindo == 0 || canalAtual == canalFavorito) {
                     try {
-                        tvSemaphore.acquire(); // Adquire acesso à TV
+                        tvSemaphore.acquire();
                         espectadoresAssistindo++;
                         canalAtual = canalFavorito;
-                        System.out.println(nome + " ligou a TV no canal " + canalAtual + " e está assistindo por " + tempoAssistindoTv + " segundos.");
+                        System.out.println(id + " ligou a TV no canal " + canalAtual + " e está assistindo por " + tempoAssistindoTv + " segundos.");
                         conseguiuAssistir = true;
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 } else {
-                    System.out.println(nome + " está esperando que a TV fique disponível para mudar para o canal " + canalFavorito + ".");
+
+                    System.out.println(id + " está esperando que a TV fique disponível para mudar para o canal " + canalFavorito + ".");
                     try {
                         Hospede.class.wait();
                     } catch (InterruptedException e) {
@@ -49,34 +52,38 @@ public class Hospede extends Thread {
             }
         }
 
+
         try {
             Thread.sleep(tempoAssistindoTv * 1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         } finally {
+
             synchronized (Hospede.class) {
                 espectadoresAssistindo--;
                 if (espectadoresAssistindo == 0) {
-                    System.out.println(nome + " foi o último a sair da TV. TV desligada.");
+                    System.out.println(id + " foi o último a sair da TV. TV desligada.");
                     canalAtual = -1;
                     tvSemaphore.release();
-                    Hospede.class.notifyAll(); // Notifica os outros hóspedes que a TV está disponível
+                    Hospede.class.notifyAll();
                 } else {
-                    System.out.println(nome + " parou de assistir TV. Ainda há " + espectadoresAssistindo + " pessoa(s) assistindo.");
+                    System.out.println(id + " parou de assistir TV. Ainda há " + espectadoresAssistindo + " pessoa(s) assistindo.");
                 }
             }
         }
     }
 
+
     public void descansar() {
         try {
             String atividade = escolherOutraAtividade();
-            System.out.println(nome + " está " + atividade + " por " + tempoDescansando + " segundos.");
+            System.out.println(id + " está " + atividade + " por " + tempoDescansando + " segundos.");
             Thread.sleep(tempoDescansando * 1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
+
 
     private String escolherOutraAtividade() {
         String[] atividades = {"descansando", "jogando bola", "lendo", "fazendo um passeio"};
